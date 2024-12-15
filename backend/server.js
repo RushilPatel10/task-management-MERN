82baff00a813540fa2b5bb0745133c79f0936fd0
Rushil patel <patelrushil1510@gmail.com>
2024-12-16 02:01:48 +0530

    frontend deployment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: 'https://task-management-mern-frontend.onrender.com',
     credentials: true,
   })
 );
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-management-mern-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
